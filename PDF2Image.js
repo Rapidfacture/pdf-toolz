c7a289200cb688fcee1c3acbf67415fe44b32ec8
Felix Furtmayr <ff@rapidfacture.com>
2021-01-10 12:04:17 +0100

    pdf2image: build awrapper promise around execFile to make it reliable; also add a inverse function image2pdf

--- PDF2Image.js
+++ PDF2Image.js
@@ -1,15 +1,15 @@
 const fs = require('mz/fs');
 const path = require('path');
 const _ = require('lodash');
-const WithTmpDir = require('with-tmp-dir-promise').WithTempDir;
-const exec = require('mz/child_process').execFile;
+const withTmpDir = require('with-tmp-dir-promise').WithTempDir;
+// const exec = require('mz/child_process').execFile;
 const {requireNativeExecutableSync} = require('require-native-executable');
 const { execFile } = require('child_process');
 
 requireNativeExecutableSync('gm');
 
 async function pdfToImage (pdfBuf, imgtype = 'jpg', dpi = 200) {
-    return WithTmpDir(async (tmpdir) => {
+    return withTmpDir(async (tmpdir) => {
         const srcpath = path.join(tmpdir, 'in.pdf');
         const outpath = path.join(tmpdir, `out-%04d.${imgtype}`);
         // Write input files
@@ -17,9 +17,13 @@ async function pdfToImage (pdfBuf, imgtype = 'jpg', dpi = 200) {
         // Convert
         var cmd = `gm convert +adjoin -format ${imgtype} -density ${dpi} ${srcpath} -quality 95 ${outpath}`;
         cmd = cmd.split(' ');
-        await execFile(cmd[0], cmd.slice(1));
+        // execute the command reliable async using a promise
+        await new Promise((resolve, reject) => {
+            execFile(cmd[0], cmd.slice(1), execThenFunct(resolve, reject));
+        });
         // Read all the files
         const files = await fs.readdir(tmpdir);
+        // console.log('files', files);
         const imgs = files.filter(file => _.endsWith(file, `.${imgtype}`)).sort();
         const imgPaths = imgs.map(img => path.join(tmpdir, img));
         const imgPromises = imgPaths.map(img => fs.readFile(img));
@@ -27,8 +31,46 @@ async function pdfToImage (pdfBuf, imgtype = 'jpg', dpi = 200) {
     }, { unsafeCleanup: true, prefix: 'pdftoolz-pdf2img-' });
 }
 
-// test().then(console.log).catch(console.error);
+
+async function imageToPdf (buffer, opts) {
+    return withTmpDir(async (tmpdir) => {
+        const srcpath = path.join(tmpdir, 'inPutImage');
+        const outpath = path.join(tmpdir, `out-%04d.pdf`);
+        // Write input files
+        await fs.writeFile(srcpath, buffer);
+        // Convert
+        var cmd = `gm convert ${srcpath} ${outpath}`;
+        cmd = cmd.split(' ');
+        // execute the command reliable async using a promise
+        await new Promise((resolve, reject) => {
+            execFile(cmd[0], cmd.slice(1), {timeout: 2000}, execThenFunct(resolve, reject));
+        });
+        // Read all the files
+        return fs.readFile(outpath);
+    }, { unsafeCleanup: true, prefix: 'pdftoolz-pdf2img-' });
+}
+
+
+/** execThenFunct
+ * help to wrapp a promise around execFile
+ * otherwise execFile returns only the childprocess, before it has finished
+ */
+function execThenFunct (resolve, reject) {
+    return function (err, data) {
+        let returnValue;
+        if (err) {
+            returnValue = 'ERROR:\n' + err;
+            reject(returnValue);
+        } else {
+            returnValue = data.toString();
+            resolve(returnValue);
+        }
+        console.log('cmd return value: ' + returnValue);
+    };
+}
+
 
 module.exports = {
-    pdfToImage: pdfToImage
+    imageToPdf,
+    pdfToImage
 };
